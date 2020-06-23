92f96735d88b774402a8d7ed1997c027e1e68c37
Rajni-Padhariya <rajnipadhariya424@gmail.com>
2020-06-23 17:11:21 +0530

    added mapping

--- downloader.py
+++ downloader.py
@@ -9,7 +9,8 @@ def create_folders():
     This function will be used to create train and test folders if not already exists
     """
     # Those folders will created to download images if not all ready exists
-    train_test_folders = ["category/train/anime-SFW", "category/train/hentai-NSFW", "category/train/neutral-SFW",
+    train_test_folders = ["category/train/drawings-SFW", "category/train/hentai-NSFW",
+                          "category/train/neutral-SFW",
                           "category/train/porn-NSFW", "category/train/sexy-SFW", "category/test/test"]
 
     for folder in train_test_folders:
@@ -23,69 +24,27 @@ def create_folders():
 def generate_urls(train_size, test_size):
     """
     this function will read data from url text files and generate corresponding list of
-    urls for each specific folder and given it specific path and unique names to
+    urls for each specific folder and give it specific path and unique names to
     avoid overwriting images and returns list of urls and names
     """
     all_urls = []
     all_names = []
+    file_names = {"drawings": "SFW", "hentai": "NSFW", "neutral": "SFW", "porn": "NSFW", "sexy": "SFW"}
 
     for file in glob.glob("raw_data/*.txt"):
-        if "drawing" in file:
+        file_name = file.replace("/", " ").replace("_", " ").replace(".", " ").split()[2]
+        if file_name in file_names:
             with open(file) as f:
                 drawing_urls = f.readlines()
                 all_urls.extend(drawing_urls[:train_size])
                 all_urls.extend(drawing_urls[train_size:train_size + test_size])
 
-                train_file_paths = ["category/train/anime-SFW/anime_{}.jpg".format(i) for i in range(train_size)]
-                test_file_paths = ["category/test/test/anime_{}.jpg".format(i) for i in range(test_size)]
+                train_file_paths = [
+                    "category/train/{}-{}/{}_{}.jpg".format(file_name, file_names[file_name], file_name, i)
+                    for i in
+                    range(train_size)]
 
-                all_names.extend(train_file_paths)
-                all_names.extend(test_file_paths)
-
-        elif "hentai" in file:
-            with open(file) as f:
-                hentai_urls = f.readlines()
-                all_urls.extend(hentai_urls[:train_size])
-                all_urls.extend(hentai_urls[train_size:train_size + test_size])
-
-                train_file_paths = ["category/train/hentai-NSFW/hentai_{}.jpg".format(i) for i in range(train_size)]
-                test_file_paths = ["category/test/test/hentai_{}.jpg".format(i) for i in range(test_size)]
-
-                all_names.extend(train_file_paths)
-                all_names.extend(test_file_paths)
-
-        elif "neutral" in file:
-            with open(file) as f:
-                neutral_urls = f.readlines()
-                all_urls.extend(neutral_urls[:train_size])
-                all_urls.extend(neutral_urls[train_size:train_size + test_size])
-
-                train_file_paths = ["category/train/neutral-SFW/neutral_{}.jpg".format(i) for i in range(train_size)]
-                test_file_paths = ["category/test/test/neutral_{}.jpg".format(i) for i in range(test_size)]
-
-                all_names.extend(train_file_paths)
-                all_names.extend(test_file_paths)
-
-        elif "porn" in file:
-            with open(file) as f:
-                porn_urls = f.readlines()
-                all_urls.extend(porn_urls[:train_size])
-                all_urls.extend(porn_urls[train_size:train_size + test_size])
-
-                train_file_paths = ["category/train/porn-NSFW/porn_{}.jpg".format(i) for i in range(train_size)]
-                test_file_paths = ["category/test/test/porn_{}.jpg".format(i) for i in range(test_size)]
-
-                all_names.extend(train_file_paths)
-                all_names.extend(test_file_paths)
-
-        elif "sexy" in file:
-            with open(file) as f:
-                sexy_urls = f.readlines()
-                all_urls.extend(sexy_urls[:train_size])
-                all_urls.extend(sexy_urls[train_size:train_size + test_size])
-
-                train_file_paths = ["category/train/sexy-SFW/sexy_{}.jpg".format(i) for i in range(train_size)]
-                test_file_paths = ["category/test/test/sexy_{}.jpg".format(i) for i in range(test_size)]
+                test_file_paths = ["category/test/test/{}_{}.jpg".format(file_name, i) for i in range(test_size)]
 
                 all_names.extend(train_file_paths)
                 all_names.extend(test_file_paths)
@@ -97,6 +56,7 @@ class QuoteGetter(Thread):
     """
     Create QuoteGetter class which will inherit from Thread class
     """
+
     def __init__(self, url, name):
         super().__init__()
         self.data = None
@@ -132,7 +92,7 @@ def main():
     and download images in train and test folder to train and test model
     """
     # Specify number of images for download in train and test folders
-    all_urls, all_names = generate_urls(train_size=1000, test_size=200)
+    all_urls, all_names = generate_urls(train_size=10, test_size=2)
 
     # This function will create train and test folders if not all ready exists
     create_folders()
